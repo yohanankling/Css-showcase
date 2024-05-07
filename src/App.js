3c124b6ab0e62990c32d1fc3482e23dfabe2bb8c
yohanan <yohanankli@gmail.com>
2024-05-07 16:44:46 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Sidebar from "./components/sidebar/sidebar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Info from './pages/Info';
+import Menu from "./assets/menu.png";
+
 
 function App() {
     const [selectedCategories, setSelectedCategories] = useState(['text', 'hover', 'button']);
+    const [showSidebar, setShowSidebar] = useState(true);
 
     return (
         <Router>
@@ -21,9 +24,12 @@ function App() {
                         <Route path="/" element={<Home selectedCategories={selectedCategories} />} />
                     </Routes>
                 </div>
-                <div className={"sidebar"}>
+                <div className={`sidebar ${showSidebar ? '' : 'hidden'}`}>
                     <Sidebar setSelectedCategories={setSelectedCategories} />
                 </div>
+                <button className="toggle-button" onClick={() => setShowSidebar(!showSidebar)}>
+                    <img src={Menu} alt="Menu" style={{ width: '20px', height: '20px' }}/>
+                </button>
             </div>
         </Router>
     );
