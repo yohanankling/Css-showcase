d78908f2778d3a2089aee3d43768069dd0b6eaa9
yohanan <yohanankli@gmail.com>
2024-05-08 12:26:12 +0300

    mobile view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Header from "./components/header/header";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./components/sidebar/sidebar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Info from './pages/Info';
 import Menu from "./assets/menu.png";
 
-
 function App() {
     const [selectedCategories, setSelectedCategories] = useState(['text', 'hover', 'button']);
-    const [showSidebar, setShowSidebar] = useState(true);
+    const [showSidebar, setShowSidebar] = useState(window.innerWidth > 500);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setShowSidebar(window.innerWidth > 499);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <Router>
@@ -18,7 +26,7 @@ function App() {
                 <header className="App-header">
                     <Header />
                 </header>
-                <div className={"body"}>
+                <div className={`body ${showSidebar && window.innerWidth <499 ? 'blurred-background' : ''}`}>
                     <Routes>
                         <Route path="/info" element={<Info />} />
                         <Route path="/" element={<Home selectedCategories={selectedCategories} />} />
@@ -27,8 +35,10 @@ function App() {
                 <div className={`sidebar ${showSidebar ? '' : 'hidden'}`}>
                     <Sidebar setSelectedCategories={setSelectedCategories} />
                 </div>
-                <button className="toggle-button" onClick={() => setShowSidebar(!showSidebar)}>
-                    <img src={Menu} alt="Menu" style={{ width: '20px', height: '20px' }}/>
+                <button
+                    className="toggle-button"
+                    onClick={() => setShowSidebar((prevState) => !prevState)}>
+                    <img src={Menu} alt="Menu" style={{ width: '20px', height: '20px' }} />
                 </button>
             </div>
         </Router>
